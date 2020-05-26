a3e6e7c751b379c4d3e7f52edee176a10cc00729
Dmitry Kozhedubov <hiisi13@gmail.com>
2020-05-26 16:37:46 +0300

    Multichannel REST implementation + some validation

--- cli.py
+++ cli.py
@@ -14,7 +14,7 @@ recognitionClient = RecognitionClient(
 )
 
 
-def recognize(language, model, response_type, filename):
+def recognize(language, model, response_type, filename, audio_channel_count=1):
     with open(filename, "rb") as f:
         content = f.read()
 
@@ -22,7 +22,8 @@ def recognize(language, model, response_type, filename):
         'language': language,
         'model': model,
         'encoding': enums.AudioEncoding.WAV,
-        'response_type': response_type
+        'response_type': response_type,
+        'audio_channel_count': audio_channel_count
     }
     return recognitionClient.recognize(config, content)
 
@@ -49,6 +50,21 @@ def recognize_word_list(language, model, filename):
     result = recognize(language, model, enums.ResponseType.WORD_LIST, filename)
     for w in result:
         print(w.word)
+    return 0
+
+
+@cli.command()
+@click.option('--language', default=enums.Language.RU)
+@click.option('--model', default=enums.Model.GENERAL)
+@click.option('--filename', type=str,)
+@click.option('--audio_channel_count', type=int,)
+def recognize_multichannel(language, model, filename, audio_channel_count):
+    result = recognize(language, model, enums.ResponseType.MULTICHANNEL, filename, audio_channel_count)
+    for ch in result:
+        print(f'Channel {ch.channel_id}')
+        for w in ch.result:
+            print(w.word)
+        print('\n')
     return 0
 
 
--- speechpro/cloud/speech/recognition/rest/__init__.py
+++ speechpro/cloud/speech/recognition/rest/__init__.py
@@ -1,16 +1,22 @@
 import base64
 
 from speechpro.cloud.speech.recognition.rest.cloud_client.api import RecognizeApi
-from speechpro.cloud.speech.recognition.rest.cloud_client.models import AudioFileDto, RecognitionRequestDto
+from speechpro.cloud.speech.recognition.rest.cloud_client.models import AudioFileDto, RecognitionRequestDto, AdvancedRecognitionRequestDto
 
 from speechpro.cloud.speech.recognition import enums
+
+
+CONFIG_MODEL_KEY = 'model'
+
 
 class ShortAudioRecognitionClient():
 
     model_mapping = {
         (enums.Language.RU, enums.Model.GENERAL): 'FarField',
         (enums.Language.RU, enums.Model.PHONE_CALL): 'TelecomRus',
-        (enums.Language.KZ, enums.Model.PHONE_CALL): 'TelecomKz'
+        (enums.Language.KZ, enums.Model.PHONE_CALL): 'TelecomKz',
+        (enums.Language.EN, enums.Model.PHONE_CALL): 'TelecomEngUs',
+        (enums.Language.ES, enums.Model.PHONE_CALL): 'TelecomEsp'
     }
 
 
@@ -19,17 +25,36 @@ class ShortAudioRecognitionClient():
         audio_str = str(b64encoded_audio, 'ascii', 'ignore')
 
         recognize_api = RecognizeApi()
-        audio_file = AudioFileDto(audio_str, config['encoding'].value)
+        audio_file = AudioFileDto(audio_str, config.get('encoding', enums.AudioEncoding.WAV).value)
 
-        model = config['model'] if isinstance(config['model'], enums.Model) else enums.Model[config['model']]
+        try:
+            model = config[CONFIG_MODEL_KEY] \
+                        if isinstance(config[CONFIG_MODEL_KEY], enums.Model) \
+                        else enums.Model[config[CONFIG_MODEL_KEY]]
+        except:
+            raise ValueError(f"Model is not provided or does not exist. Available models: {' '.join([m.name for m in enums.Model])}")
+
+        try:
+            mapped_model = self.model_mapping[(config.get('language', enums.Language.RU), model)]
+        except:
+            raise ValueError('Provided language is not supported')
+
         recognition_request = RecognitionRequestDto(
-            audio_file, self.model_mapping[(config['language'], model)]
+            audio_file, mapped_model
         )
-        response_type = config.get('response_type')
+        response_type = config.get('response_type', enums.ResponseType.PLAIN_TEXT)
         if response_type == enums.ResponseType.PLAIN_TEXT:
             return recognize_api.recognize(self.session_id, recognition_request)
         elif response_type == enums.ResponseType.WORD_LIST:
             return recognize_api.recognize_words(self.session_id, recognition_request)
+        elif response_type == enums.ResponseType.MULTICHANNEL:
+            channel_count = config.get('audio_channel_count', 1)
+            return recognize_api.recognize_advanced(
+                self.session_id,
+                AdvancedRecognitionRequestDto(
+                    mapped_model, channels=[i for i in range(0, channel_count)], data=audio_str
+                )
+            )
 
 
 class LongRunningRecognitionClient():
